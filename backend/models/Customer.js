77849ddf067c9319a45b67092b73336a1d248346
Adam Hassan <122500358+Tigger-1@users.noreply.github.com>
2024-03-27 20:11:08 +0200

    Delete backend directory

--- backend/models/Customer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-
-const CustomerSchema = new mongoose.Schema({
-    
-
-});
-
-module.exports = mongoose.model("Customer", CustomerSchema);
--- backend/models/UserType.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-
-const UserTypeSchema = new mongoose.Schema({
-    
-
-});
-
-module.exports = mongoose.model("User Type", UserTypeSchema);
