089442d6b4bd0fe82e8920d944133384bb47c97d
Adam Hassan <adamhassan011003@gmail.com>
2024-03-27 20:13:46 +0200

    backend
    uploading the backend files

---
+++ backend/models/OrderType.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+
+const OrderTypeSchema = new mongoose.Schema({
+    
+
+});
+
+module.exports = mongoose.model("Order Type", OrderTypeSchema);
---
+++ backend/models/UserType.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+
+const UserTypeSchema = new mongoose.Schema({
+    
+
+});
+
+module.exports = mongoose.model("User Type", UserTypeSchema);
