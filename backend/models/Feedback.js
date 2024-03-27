b16f066306e7248fa27b005d4217529d86b5c4b2
Adam Hassan <adamhassan011003@gmail.com>
2024-03-27 19:59:13 +0200

    first transport of project
    transferring the first half of the base project files

---
+++ backend/models/Feedback.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+
+const FeedbackSchema = new mongoose.Schema({
+    
+
+});
+
+module.exports = mongoose.model("Feedback", FeedbackSchema);
---
+++ backend/models/MenuType.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+
+const MenuTypeSchema = new mongoose.Schema({
+    
+
+});
+
+module.exports = mongoose.model("Menu Type", MenuTypeSchema);
